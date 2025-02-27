42622f02afb7ab971ee4594d865a2d81b3bba52d
ColbyDomingos <colbydomingos@my.whccd.edu>
2025-02-26 21:03:15 -0800

    added xml comments and single comments

--- COMP003A.Final/TMB.cs
+++ COMP003A.Final/TMB.cs
@@ -6,11 +6,20 @@ using System.Threading.Tasks;
 
 namespace COMP003A.Final
 {
+    /// <summary>
+    /// This is the base class meant to be changedby the derived classes
+    /// </summary>
     public class TMB : ITO
     {
         protected List<Task> Tasks = new List<Task>();
         private int taskIDCounter = 1;
 
+        /// <summary>
+        /// This is the addTask method which adds a task and has a max of 50, more than 50 might just be torture
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="description"></param>
+        /// <param name="dueDate"></param>
         public void AddTask(string title, string description, DateTime dueDate)
         {
             try
@@ -35,62 +44,78 @@ namespace COMP003A.Final
             }
         }
 
-
+        /// <summary>
+        /// This is the update task method where you can of course, update a task
+        /// </summary>
+        /// <param name="taskID"></param>
+        /// <param name="newTitle"></param>
+        /// <param name="newDescription"></param>
+        /// <param name="newDueDate"></param>
         public void UpdateTask(int taskID, string newTitle, string newDescription, DateTime newDueDate)
         {
             try
             {
-                Task task = Tasks.Find(t => t.ID == taskID);
-                if (task != null)
+                Task task = Tasks.Find(t => t.ID == taskID); //finds the task ID from the tasks list
+                if (task != null) //if the task is real
                 {
                     task.Title = newTitle;
-                    task.Description = newDescription;
+                    task.Description = newDescription; //change all the parts of the task
                     task.DueDate = newDueDate;
                 }
 
                 else
                 {
-                    Console.WriteLine("\nTASK WASN'T FOUND");
+                    Console.WriteLine("\nTASK WASN'T FOUND"); //if not then no task is found
                 }
             }
 
             catch(Exception ex)
             {
-                Console.WriteLine("Error when updating task: " + ex.Message);
+                Console.WriteLine("Error when updating task: " + ex.Message);//if there was something wrong with the input it will give a message
             }
         }
 
 
+        /// <summary>
+        /// This is the remove task method where you can remove a task!!!!!!!
+        /// </summary>
+        /// <param name="taskID"></param>
         public void RemoveTask(int taskID)
         {
             try
             {
-                Task task = Tasks.Find(t => t.ID == taskID);
-                if (task != null)
+                Task task = Tasks.Find(t => t.ID == taskID); //find the task in the tasks list
+                if (task != null) //if there is a task with that ID
                 {
-                    Tasks.Remove(task);
+                    Tasks.Remove(task); //use the list .remove to remove the task
                 }
 
                 else
                 {
-                    Console.WriteLine("Task was not Found!!!");
+                    Console.WriteLine("Task was not Found!!!"); //if not then there is not a task there
                 }
             }
 
             catch (Exception ex)
             {
-                Console.WriteLine("Cannot remove task!: " + ex.Message);
+                Console.WriteLine("Cannot remove task!: " + ex.Message); //in case there is an issue with the input at any point
             }
         }
 
+
+
+        /// <summary>
+        /// This marks a task as complete, pretty cool
+        /// </summary>
+        /// <param name="taskID"></param>
         public void MarkTaskComplete(int taskID)
         {
             try
             {
-                Task task = Tasks.Find(t => t.ID == taskID);
+                Task task = Tasks.Find(t => t.ID == taskID); //find the task in the list
                 if (task != null)
                 {
-                    task.IsCompleted = true;
+                    task.IsCompleted = true; //mark task as complete changing the bool for it
                 }
                 else
                 {
