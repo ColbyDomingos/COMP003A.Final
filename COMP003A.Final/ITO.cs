6bd62ae42c3f27d557b4c1a887863a339ddb689a
ColbyDomingos <colbydomingos@my.whccd.edu>
2025-02-26 19:55:27 -0800

    Add project files.

---
+++ COMP003A.Final/ITO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.Final
+{
+    public interface ITO
+    {
+        void AddTask(string title, string description, DateTime dueDate);
+
+        void UpdateTask(int taskID, string newTitle, string newDescription, DateTime newDueDate);
+
+        void RemoveTask(int taskID);
+
+        void MarkTaskCompleted(int taskID);
+
+        void DisplayTasks();
+    }
+}
