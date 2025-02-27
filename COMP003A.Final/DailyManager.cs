1a06dbda41e3c802d035ba690ec5e3edfbbc0d3b
ColbyDomingos <colbydomingos@my.whccd.edu>
2025-02-26 22:07:49 -0800

    Continued onto the main program class and added a basic implementation and testing

---
+++ COMP003A.Final/DailyManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.Final
+{
+    public class DailyManager : TMB
+    {
+        public override void DisplayTasks()
+        {
+            Console.WriteLine("Daily Task List");
+            base.DisplayTasks();
+        }
+    }
+}
--- COMP003A.Final/Program.cs
+++ COMP003A.Final/Program.cs
@@ -3,13 +3,74 @@
 //Faculty: Jonathan Rodrigo Cruz
 //Description: Final project for the course COMP003A
 
+using System.Transactions;
+
 namespace COMP003A.Final
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Select Task Manager: \n 1. Personal \n 2. Work \n 3.Daily ");
+            int choice = int.Parse(Console.ReadLine());
+
+            TMB taskManager;
+            switch (choice)
+            {
+                case 1: 
+                    taskManager = new PersonalManager(); 
+                    break;
+                case 2:
+                    taskManager = new WorkManager();
+                    break;
+                case 3:
+                    taskManager = new DailyManager();
+                    break;
+                default:
+                    Console.WriteLine("Invalid, defaulting to the Personal Manager");
+                    taskManager = new PersonalManager();
+                        break;
+            }
+
+            while (true)
+            {
+                Console.WriteLine("|--Menu--|");
+                Console.WriteLine("1. Add");
+                Console.WriteLine("2. Update");
+                Console.WriteLine("3. Remove");
+                Console.WriteLine("4. Mark Complete");
+                Console.WriteLine("5. Display");
+                Console.WriteLine("6. Exit");
+
+                int option = int.Parse(Console.ReadLine());
+
+                try
+                {
+                    if (option == 1)
+                    {
+                        Console.Write("Enter a Title: ");
+                        string title = Console.ReadLine();
+                        Console.WriteLine("Enter Description: ");
+                        string description = Console.ReadLine();
+                        Console.WriteLine("Enter the due date (year-mo-da): ");
+                        DateTime dueDate = DateTime.Parse(Console.ReadLine());
+                        taskManager.AddTask(title, description, dueDate);
+                    }
+
+                    else if (option == 2)
+                    {
+                        Console.WriteLine("Enter the task ID: ");
+                        int ID = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter the new title: ");
+                        string newTitle = Console.ReadLine();
+                        Console.WriteLine("Enter the new description: ");
+                        string newDescription = Console.ReadLine();
+                        Console.WriteLine("Enter the new Due Date (year-me-da): ");
+                        DateTime newDueDate = DateTime.Parse(Console.ReadLine());
+                        taskManager.UpdateTask(ID, newTitle, newDescription, newDueDate)
+                    }
+                }
+            }
         }
     }
 }
